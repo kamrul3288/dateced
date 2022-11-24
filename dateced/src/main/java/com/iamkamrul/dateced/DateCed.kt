1725b64fd79f6b9cf68d0ce623536f930582d90d
KamrulHasan <kamrulhasan3288@gmail.com>
2022-11-25 01:27:07 +0600

    implement input date time pattern matching

--- dateced/src/main/java/com/iamkamrul/dateced/DateCed.kt
+++ dateced/src/main/java/com/iamkamrul/dateced/DateCed.kt
@@ -1,5 +1,24 @@
 package com.iamkamrul.dateced
 
-class DateCed(private val dateString : String = "") {
+import java.util.*
+
+private typealias SimpleDateFormatPattern = String
+class DateCed(private val dateTimeString : String = "") {
+    private val _dateTimeString = dateTimeString.replaceInput()
+    private var dateTime: Date? = null
+
+
+    private fun matchPattern(_input:String):SimpleDateFormatPattern{
+        return if (matchYMDStringDateAndTime(_input)){
+            "yyyy-MM-dd hh:mm:ss"
+        }else if (matchYMDStringDate(_input)){
+            "yyyy-MM-dd"
+        }else if (matchDMYStringDateAndTime(_input)){
+            "dd-MM-yyyy hh:mm:ss"
+        }else if (matchDMYStringDate(_input)){
+            "dd-MM-yyyy"
+        }else throw IllegalArgumentException("Error Occurred! $_dateTimeString: Format incorrect")
+    }
 
 }
+private fun String.replaceInput():String = this.replace("/","-")
