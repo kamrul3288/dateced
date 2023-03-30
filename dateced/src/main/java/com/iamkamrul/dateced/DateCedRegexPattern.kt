d7b845d6275cf383ef597fba2845d01b48070477
KamrulHasan <kamrulhasan3288@gmail.com>
2023-03-30 15:18:29 +0600

    add pair pattern regex

--- dateced/src/main/java/com/iamkamrul/dateced/DateCedRegexPattern.kt
+++ dateced/src/main/java/com/iamkamrul/dateced/DateCedRegexPattern.kt
@@ -3,32 +3,43 @@ package com.iamkamrul.dateced
 private typealias SimpleDateFormatPattern = String
 
 internal object DateCedPattern {
-    fun matchPattern(_input:String):SimpleDateFormatPattern{
-        return if (matchYMDStringDateAndTime(_input)){
-            "yyyy-MM-dd HH:mm:ss"
-        }else if (matchYMDStringDate(_input)){
-            "yyyy-MM-dd"
-        }else if (matchDMYStringDateAndTime(_input)){
-            "dd-MM-yyyy HH:mm:ss"
-        }else if (matchDMYStringDate(_input)){
-            "dd-MM-yyyy"
-        }else throw IllegalArgumentException("Error Occurred! $_input: Format incorrect")
+    fun matchPattern(input:String):SimpleDateFormatPattern{
+        patterns.forEach {
+            if (it.first.matches(input)) return it.second
+        }
+        throw IllegalArgumentException("Error Occurred! $input: Format incorrect")
     }
 
 }
 
-private fun  matchYMDStringDateAndTime(input:String):Boolean{
-    return Regex("\\d{4}-\\d{1,2}-\\d{1,2} \\d{1,2}:\\d{1,2}:\\d{1,2}$").matches(input)
-}
+private val patterns = listOf(
+    Pair(first = """^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$""".toRegex(), second = "yyyy-MM-dd HH:mm:ss"),
+    Pair(first = """^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$""".toRegex(), second = "yyyy-MM-dd HH:mm"),
+    Pair(first = """^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \d{2}$""".toRegex(), second = "yyyy-MM-dd HH:mm:ss aa"),
+    Pair(first = """^\d{4}-\d{2}-\d{2} \d{2}:\d{2} \d{2}$""".toRegex(), second = "yyyy-MM-dd HH:mm aa"),
 
-private fun matchYMDStringDate(input:String):Boolean{
-    return Regex("\\d{4}-\\d{1,2}-\\d{1,2}\$").matches(input)
-}
+    Pair(first = """^\d{4}-\d{2}-\d{2} \d{2}$""".toRegex(), second = "yyyy-MM-dd"),
+    Pair(first = """^\d{2}-\d{2}-\d{2} \d{4}$""".toRegex(), second = "dd-MM-yyyy"),
 
-private fun matchDMYStringDateAndTime(input:String):Boolean{
-    return Regex("\\d{1,2}-\\d{1,2}-\\d{4} \\d{1,2}:\\d{1,2}:\\d{1,2}$").matches(input)
-}
+    Pair(first = """^\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}$""".toRegex(), second = "dd-MM-yyyy HH:mm:ss"),
+    Pair(first = """^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$""".toRegex(), second = "dd-MM-yyyy HH:mm"),
+    Pair(first = """^\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2} \d{2}$""".toRegex(), second = "dd-MM-yyyy HH:mm:ss aa"),
+    Pair(first = """^\d{2}-\d{2}-\d{4} \d{2}:\d{2} \d{2}$""".toRegex(), second = "dd-MM-yyyy HH:mm aa"),
 
-private fun matchDMYStringDate(input:String):Boolean{
-    return Regex("\\d{1,2}-\\d{1,2}-\\d{4}\$").matches(input)
-}
+    Pair(first = """^\d{2}-\d{3}-\d{4}$""".toRegex(), second = "dd MMM yyyy"),
+    Pair(first = """^\d{2}-\d{3}-\d{4} \d{2}:\d{2}:\d{2} \d{2}$""".toRegex(), second = "dd MMM yyyy hh:mm:ss aa"),
+    Pair(first = """^\d{2}-\d{3}-\d{4} \d{2}:\d{2} \d{2}$""".toRegex(), second = "dd MMM yyyy hh:mm aa"),
+
+    Pair(first = """^\d{2}-\d{3}-\d{4} \d{2}:\d{2}:\d{2}$""".toRegex(), second = "dd MMM yyyy HH:mm:ss"),
+    Pair(first = """^\d{2}-\d{3}-\d{4} \d{2}:\d{2}$""".toRegex(), second = "dd MMM yyyy HH:mm"),
+
+
+    Pair(first = """^\d{2}:\d{2}:\d{2}$""".toRegex(), second = "HH:mm:ss"),
+    Pair(first = """^\d{2}:\d{2}$""".toRegex(), second = "HH:mm"),
+
+    Pair(first = """^\d{2}:\d{2}:\d{2} \d{2}$""".toRegex(), second = "HH:mm:ss aa"),
+    Pair(first = """^\d{2}:\d{2} \d{2}$""".toRegex(), second = "HH:mm aa"),
+
+    Pair(first = """^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$""".toRegex(), second = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
+
+)
