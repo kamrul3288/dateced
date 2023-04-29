a31884792e0e90d5b4d4791763defce0d8e33759
KamrulHasan <kamrulhasan3288@gmail.com>
2023-04-29 14:54:03 +0600

    release 1.0.5

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,6 +2,6 @@ object AppConfig {
     const val minSdkVersion = 21
     const val compileSdkVersion = 33
     const val targetSdkVersion = 33
-    const val versionCode  = 4
-    const val versionName  = "1.0.4"
+    const val versionCode  = 5
+    const val versionName  = "1.0.5"
 }
