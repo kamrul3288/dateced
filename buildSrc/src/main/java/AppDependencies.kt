dbadfc5041336f3610a32455ccfe7c5c8fe89f3e
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-09-25 14:56:31 +0600

    add(dependency): added desugar library

--- buildSrc/src/main/java/AppDependencies.kt
+++ buildSrc/src/main/java/AppDependencies.kt
@@ -1,10 +1,10 @@
 object AppDependencies {
-
-
     const val appcompat = "androidx.appcompat:appcompat:${DependenciesVersion.appcompat_version}"
     const val core_ktx = "androidx.core:core-ktx:${DependenciesVersion.core_ktx_version}"
     const val constraint_layout = "androidx.constraintlayout:constraintlayout:${DependenciesVersion.constraint_layout_version}"
     const val material = "com.google.android.material:material:${DependenciesVersion.material_version}"
+    const val kotlinxDatetime = "org.jetbrains.kotlinx:kotlinx-datetime:${DependenciesVersion.kotlinxDatetime}"
+    const val desugar = "com.android.tools:desugar_jdk_libs:${DependenciesVersion.desugar_version}"
 
 
 
--- buildSrc/src/main/java/DependenciesVersion.kt
+++ buildSrc/src/main/java/DependenciesVersion.kt
@@ -2,8 +2,9 @@ object DependenciesVersion {
     const val core_ktx_version = "1.9.0"
     const val appcompat_version = "1.6.1"
     const val material_version = "1.8.0"
+    const val desugar_version = "2.0.3"
     const val constraint_layout_version = "2.1.4"
-
+    const val kotlinxDatetime = "0.4.1"
 
     const val junit_version = "4.13.2"
     const val junit_ext_version = "1.1.3"
